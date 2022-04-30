9c6520cb42dc2080aa297fd6a0f9fb5a3017491d
mdhUser <734570993@qq.com>
2022-04-30 09:20:24 +0800

    gitignore 优化

--- springboot-webdemo/src/main/java/org/maxwell/webdemo/utils/ResponseStatus.java
+++ springboot-webdemo/src/main/java/org/maxwell/webdemo/utils/ResponseStatus.java
@@ -24,16 +24,11 @@ public enum ResponseStatus {
      * 失败 服务器端报错
      */
     FAIL(500, "failed"),
-
-
     HTTP_STATUS_200(200, "ok"),
     HTTP_STATUS_400(400, "request error"),
     HTTP_STATUS_401(401, "no authentication"),
     HTTP_STATUS_403(403, "no authorities"),
     HTTP_STATUS_500(500, "server error");
-
-    public static final List<ResponseStatus> HTTP_STATUS_ALL =
-            List.of(HTTP_STATUS_200, HTTP_STATUS_400, HTTP_STATUS_401, HTTP_STATUS_403, HTTP_STATUS_500);
 
     /**
      * response code
